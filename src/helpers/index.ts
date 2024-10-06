c9c3693ec885fa3941fb1e3afa3ca28030088a9c
Ezedike-egwom Collins <ezedikecollins@gmail.com>
2024-10-06 05:25:59 +0100

    feat: reduce mint and processing fees

--- src/helpers/index.ts
+++ src/helpers/index.ts
@@ -3,8 +3,8 @@ import { clusterApiUrl } from "@solana/web3.js";
 import { createClient } from "redis";
 
 // CONSTANTS
-export const MINT_FEE = 0.05;
-export const PROCESSING_FEE = 0.0005;
+export const MINT_FEE = 0.00001;
+export const PROCESSING_FEE = 0.00001;
 export const URL_PATH = "/api/actions";
 export const CLUSTER_URL = process.env.RPC_URL ?? clusterApiUrl("mainnet-beta");
 export const HEADERS = createActionHeaders({
